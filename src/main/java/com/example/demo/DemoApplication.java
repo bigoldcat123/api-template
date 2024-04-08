380ec31193739af23295d5e53b1f697761e5b7ee
bigoldcat123 <a1456870251@163.com>
2024-04-08 21:42:12 +0800

    main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
-		System.out.println("ab");
+		System.out.println("assssb");
 	}
 
 }
