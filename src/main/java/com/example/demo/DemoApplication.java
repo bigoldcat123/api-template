766fc14f47a4231465ade2e6419026215772941e
dalaomao <a1456870251@163.com>
2024-04-16 22:13:21 +0800

    'm2'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//sss
 // changed by another me for testing
 
 @SpringBootApplication
