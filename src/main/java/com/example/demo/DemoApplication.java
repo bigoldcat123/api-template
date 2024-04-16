df6a5d4a5408e92dca1fb8fddabdcbfff1659839
bigoldcat123 <a1456870251@163.com>
2024-04-16 22:03:59 +0800

    this would not be successed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// idont think so
 
 @SpringBootApplication
 @RestController
